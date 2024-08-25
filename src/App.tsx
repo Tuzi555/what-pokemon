4293b428b4b87fd81f5f814098b7469f39582371
tuzi555 <jakub.tuzar@gmail.com>
2024-08-25 15:30:27 +0200

    prune demo files, add dark mode and red theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { useQuery } from '@tanstack/react-query';
+import { ModeToggle } from './components/mode-toggle';
 
 async function getPikachu() {
     const url = 'https://pokeapi.co/api/v2/pokemon/25';
@@ -24,43 +22,19 @@ function Pikachu() {
     const query = useQuery({ queryKey: ['pikachu'], queryFn: getPikachu });
 
     if (!query.isSuccess) return;
-    return (
-        <a href="https://www.pokemon.com/us/pokedex/pikachu" target="_blank">
-            <img src={query.data.sprites.front_default} />
-        </a>
-    );
+    return <img className="w-48 h-48" src={query.data.sprites.front_default} />;
 }
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
+            <div className="w-full flex flex-row justify-between">
+                <div />
+                <ModeToggle />
+            </div>
+            <div className="flex flex-col justify-center w-full">
                 <Pikachu />
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from './components/theme-provider.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App />
-            <ReactQueryDevtools initialIsOpen={false} />
+            <ThemeProvider>
+                <App />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </ThemeProvider>
         </QueryClientProvider>
     </StrictMode>
 );
